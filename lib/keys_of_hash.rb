e8cbd924e933c661a670d8d939b0cbd29fcec343
yong liang <yongliang24@gmail.com>
2019-01-23 06:36:36 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,12 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    array_animal = []
+		self.collect { |animal, location|
+			if arguments.include?(location)
+				array_animal << animal
+			end
+		}
+		return array_animal
   end
 end
